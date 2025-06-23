b7045acf24f5e7bfc7296939e116b2274e7b229c
hanifubaaba <baabahanif88@gmail.com>
2025-06-23 13:52:27 +0100

    added navbar component

---
+++ furniro/src/app/Navbar/page.jsx
@@ -0,0 +1,13 @@
+export default function Navbar() {
+  return (
+    <nav>
+      <h1>Furniro</h1>
+      <ul>
+        <li>Home</li>
+        <li>Shop</li>
+        <li>About</li>
+        <li>Contact</li>
+      </ul>
+    </nav>
+  );
+}
