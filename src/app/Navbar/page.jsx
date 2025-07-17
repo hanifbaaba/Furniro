eb24f3954f68045f0a5fa2d1e1f2fa78ea6057c0
hanifubaaba <baabahanif88@gmail.com>
2025-07-17 18:54:43 +0100

    Created Navbar Component

---
+++ src/app/Navbar/page.jsx
@@ -0,0 +1,12 @@
+export default function Navbar() {
+  return (
+    <nav>
+      <h1>Furniro Store</h1>
+      <ul>
+        <li>Home</li>
+        <li>Contact</li>
+        <li>Cart</li>
+      </ul>
+    </nav>
+  );
+}
