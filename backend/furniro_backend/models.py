ce26fefd4cae5b1e320c29d6e294898e61309a86
hanifubaaba <baabahanif88@gmail.com>
2025-07-24 13:43:19 +0100

    created DB Models for product data

--- backend/furniro_backend/models.py
+++ backend/furniro_backend/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
-# Create your models here.
+class Products(models.Model):
+    name = models.CharField(max_length=100)
+    description = models.TextField()
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    color = models.CharField(max_length=100)
+    # images = models.ImageField(upload_to='')
+
+    def __str__(self):
+        return self.name
