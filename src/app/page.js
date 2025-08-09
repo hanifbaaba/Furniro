eabc092df1fbd7972c69124b44ece568c00c20ad
hanifubaaba <baabahanif88@gmail.com>
2025-08-09 14:40:37 +0100

    readded footer component

--- src/app/page.js
+++ src/app/page.js
@@ -1,4 +1,4 @@
-// import Footer from "./Components/Footer/page";
+import Footer from "./Components/Footer/page";
 import Header from "./Components/Header/page";
 import Navbar from "./Components/Navbar/page";
 import DiscoverPage from "./Pages/DiscoverPage/page";
@@ -8,18 +8,18 @@ import DiscoverPage from "./Pages/DiscoverPage/page";
 // import ProductsCard from "./Pages/ProductsCard/page";
 // import Recommendation from "./Pages/Recommendation/page";
 // import products from "/products.json";
-import Link from "next/link";
+// import Link from "next/link";
 export default function HomePage() {
   return (
     <>
       <Navbar />
       <Header />
       <DiscoverPage />
+      <Footer />
       {/* <Recommendation /> */}
       {/* <ProductPage /> */}
       {/* <ProductsCard /> */}
 
-      {/* <Footer /> */}
       {/* <ProductList products={products} /> */}
       {/* <ProductList /> */}
       {/* <Recommendation /> */}
