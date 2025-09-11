655517dbba336165c7213c2f6ba48da95eb472ae
hanifubaaba <baabahanif88@gmail.com>
2025-09-11 05:35:53 +0100

    Configured endpoints

--- furniro/backend/furniro_backend/urls.py
+++ furniro/backend/furniro_backend/urls.py
@@ -1,9 +1,23 @@
-from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
-from views import UserRegistrationView
 from django.urls import path, include
+from rest_framework.routers import DefaultRouter
+from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
+from .views import (
+    UserRegistrationView,
+    ProductViewSet,
+    CartViewSet,
+    OrderItemViewSet,
+    OrderViewSet,
+)
+
+router = DefaultRouter()
+router.register(r'products', ProductViewSet, basename='product')
+router.register(r'cart', CartViewSet, basename='cart')
+router.register(r'orders', OrderViewSet, basename='order')
+router.register(r'order-items', OrderItemViewSet, basename='orderitem')
 
 urlpatterns = [
     path("api/register/", UserRegistrationView.as_view(), name="register"),
     path("api/login/", TokenObtainPairView.as_view(), name="token_obtain_pair"),
     path("api/refresh/", TokenRefreshView.as_view(), name="token_refresh"),
+    path("api/", include(router.urls)),  
 ]
