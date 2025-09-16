abbf20722a1be7dd2cb0c643432a02668a7cac2a
hanifubaaba <baabahanif88@gmail.com>
2025-09-16 05:50:22 +0100

    configured allowed hosts

--- furniro/backend/backend/settings.py
+++ furniro/backend/backend/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = os.getenv("DJANGO_SECRET_KEY", "fallback-key")
 # SECURITY WARNING: don't run with debug turned on in production!
 
 DEBUG = os.getenv("DEBUG", "False") == "True"
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["https://furniro-7nnb.onrender.com"]
 
 
 # Application definition
